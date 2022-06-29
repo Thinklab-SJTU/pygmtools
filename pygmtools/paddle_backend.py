73d00a9a5e95519ed2eb125d80b0afea90776ce3
Butcher <1065543666@sjtu.edu.cn>
2022-06-29 23:20:17 +0800

    complete rewriting functions in classic_solvers

--- pygmtools/paddle_backend.py
+++ pygmtools/paddle_backend.py
@@ -122,7 +122,120 @@ def sinkhorn(s: paddle.Tensor, nrows: paddle.Tensor=None, ncols: paddle.Tensor=N
                 ret_log_s[b, ori_nrows[b]:nrows[b], :ncols[b]] = -float('inf')
 
         if transposed:
-            ret_log_s = ret_log_s.transpose(1, 2)
+            ret_log_s = paddle.transpose(ret_log_s, perm=(0, 2, 1))
 
         return paddle.exp(ret_log_s)
 
+
+#############################################
+#    Quadratic Assignment Problem Solvers   #
+#############################################
+
+
+def rrwm(K: paddle.Tensor, n1: paddle.Tensor, n2: paddle.Tensor, n1max, n2max, x0: paddle.Tensor,
+         max_iter: int, sk_iter: int, alpha: float, beta: float) -> paddle.Tensor:
+    """
+    Paddle implementation of RRWM algorithm.
+    """
+    batch_num, n1, n2, n1max, n2max, n1n2, v0 = _check_and_init_gm(K, n1, n2, n1max, n2max, x0)
+    # rescale the values in K
+    d = paddle.sum(K, axis=2, keepdim=True)
+    dmax = paddle.max(d, axis=1, keepdim=True)
+    K = K / (dmax + paddle.min(d) * 1e-5)
+    v = v0
+    for i in range(max_iter):
+        # random walk
+        v = paddle.bmm(K, v)
+        last_v = v
+        n = paddle.norm(v, p=1, axis=1, keepdim=True)
+        v = v / n
+
+        # reweighted jump
+        s = paddle.reshape(v, (batch_num, n2max, n1max)).transpose([0, 2, 1])
+        s = beta * s / s.max(axis=1, keepdim=True).max(axis=2, keepdim=True)
+        v = paddle.reshape(alpha * sinkhorn(s, n1, n2, max_iter=sk_iter).transpose([0, 2, 1]),(batch_num, n1n2, 1)) + \
+            (1 - alpha) * v
+        n = paddle.norm(v, p=1, axis=1, keepdim=True)
+        v = paddle.matmul(v, 1 / n)
+
+        if paddle.norm(v - last_v) < 1e-5:
+            break
+
+    return v.view(batch_num, n2max, n1max).transpose(1, 2)
+
+
+def sm(K: paddle.Tensor, n1: paddle.Tensor, n2: paddle.Tensor, n1max, n2max, x0: paddle.Tensor,
+       max_iter: int) -> paddle.Tensor:
+    """
+    Paddle implementation of SM algorithm.
+    """
+    batch_num, n1, n2, n1max, n2max, n1n2, v0 = _check_and_init_gm(K, n1, n2, n1max, n2max, x0)
+    v = vlast = v0
+    for _ in range(max_iter):
+        v = paddle.bmm(K, v)
+        n = paddle.norm(v, p=2, axis=1)
+        v = paddle.reshape(paddle.matmul(v, (1 / n)), (batch_num, 1, 1))
+        if paddle.norm(v - vlast) < 1e-5:
+            break
+        vlast = v
+
+    x = paddle.reshape(v, (batch_num, n2max, n1max)).transpose((0, 2, 1))
+    return x
+
+
+def ipfp(K: paddle.Tensor, n1: paddle.Tensor, n2: paddle.Tensor, n1max, n2max, x0: paddle.Tensor,
+         max_iter) -> paddle.Tensor:
+    """
+    Pytorch implementation of IPFP algorithm
+    """
+    batch_num, n1, n2, n1max, n2max, n1n2, v0 = _check_and_init_gm(K, n1, n2, n1max, n2max, x0)
+    v = v0
+    last_v = v
+
+    def comp_obj_score(v1, K, v2):
+        return paddle.bmm(paddle.bmm(paddle.reshape(v1, (batch_num, 1, -1)), K), v2)
+
+    for i in range(max_iter):
+        cost = paddle.reshape(paddle.bmm(K, v),(batch_num, n2max, n1max)).transpose((0, 2, 1))
+        binary_sol = hungarian(cost, n1, n2)
+        binary_v = paddle.reshape(binary_sol.transpose((0, 2, 1)),(batch_num, -1, 1))
+        alpha = comp_obj_score(v, K, binary_v - v)  # + torch.mm(k_diag.view(1, -1), (binary_sol - v).view(-1, 1))
+        beta = comp_obj_score(binary_v - v, K, binary_v - v)
+        t0 = alpha / beta
+        v = paddle.where(paddle.logical_or(beta <= 0, t0 >= 1), binary_v, v + t0 * (binary_v - v))
+        last_v_sol = comp_obj_score(last_v, K, last_v)
+        if paddle.max(paddle.abs(
+                last_v_sol - paddle.bmm(paddle.reshape(cost,(batch_num, 1, -1)), paddle.reshape(binary_sol, (batch_num, -1, 1)))
+        ) / last_v_sol) < 1e-3:
+            break
+        last_v = v
+
+    pred_x = binary_sol
+    return pred_x
+
+
+def _check_and_init_gm(K, n1, n2, n1max, n2max, x0):
+    # get batch number
+    batch_num = K.shape[0]
+    n1n2 = K.shape[1]
+
+    # get values of n1, n2, n1max, n2max and check
+    if n1 is None:
+        n1 = paddle.to_tensor(paddle.full((batch_num,), n1max, dtype=paddle.int32), place=K.place)
+    if n2 is None:
+        n2 = paddle.to_tensor(paddle.full((batch_num,), n2max, dtype=paddle.int32), place=K.place)
+    if n1max is None:
+        n1max = paddle.max(n1)
+    if n2max is None:
+        n2max = paddle.max(n2)
+
+    assert n1max * n2max == n1n2, 'the input size of K does not match with n1max * n2max!'
+
+    # initialize x0 (also v0)
+    if x0 is None:
+        x0 = paddle.to_tensor(paddle.zeros((batch_num, n1max, n2max), dtype=K.dtype), place=K.place)
+        for b in range(batch_num):
+            x0[b, 0:n1[b], 0:n2[b]] = paddle.to_tensor(1.) / (n1[b] * n2[b])
+
+    v0 = paddle.reshape(paddle.transpose(x0, perm=(0, 2, 1)), (batch_num, n1n2, 1))
+    return batch_num, n1, n2, n1max, n2max, n1n2, v0
