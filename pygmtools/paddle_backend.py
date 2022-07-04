a934d91c98ccbde95cf7ed4ae4ba9cf5f01390f7
Butcher <1065543666@sjtu.edu.cn>
2022-07-04 20:06:16 +0800

    remove codes in comments that are used during developing

--- pygmtools/paddle_backend.py
+++ pygmtools/paddle_backend.py
@@ -408,48 +408,6 @@ def _aff_mat_from_node_edge_aff(node_aff: paddle.Tensor, edge_aff: paddle.Tensor
         ks.append(k)
     return paddle.stack(ks, axis=0)
 
-    # if edge_aff is not None:
-    #     place = edge_aff.place
-    #     dtype = edge_aff.dtype
-    #     batch_size = edge_aff.shape[0]
-    #     if n1 is None:
-    #         n1 = paddle.to_tensor(np.amax(connectivity1.numpy(), axis=(1, 2)).copy() + 1)
-    #     if n2 is None:
-    #         n2 = paddle.to_tensor(np.amax(connectivity2.numpy(), axis=(1, 2)).copy() + 1)
-    #     if ne1 is None:
-    #         ne1 = [edge_aff.shape[1]] * batch_size
-    #     if ne2 is None:
-    #         ne2 = [edge_aff.shape[1]] * batch_size
-    # else:
-    #     place = node_aff.place
-    #     dtype = node_aff.dtype
-    #     batch_size = node_aff.shape[0]
-    #     if n1 is None:
-    #         n1 = [node_aff.shape[1]] * batch_size
-    #     if n2 is None:
-    #         n2 = [node_aff.shape[2]] * batch_size
-
-    # n1max = max(n1)
-    # n2max = max(n2)
-    # ks = []
-    # for b in range(batch_size):
-    #     k = paddle.to_tensor(np.zeros((n2max, n1max, n2max, n1max)), dtype=dtype, place=place)
-    #     # edge-wise affinity
-    #     if edge_aff is not None:
-    #         conn1 = connectivity1[b][:ne1[b]].numpy()
-    #         conn2 = connectivity2[b][:ne2[b]].numpy()
-    #         edge_indices = paddle.to_tensor(np.concatenate([conn1.repeat(ne2[b], axis=0), np.tile(conn2, (ne1[b], 1))], axis=1)) # indices: start_g1, end_g1, start_g2, end_g2
-    #         edge_indices = (edge_indices[:, 2], edge_indices[:, 0], edge_indices[:, 3], edge_indices[:, 1]) # indices: start_g2, start_g1, end_g2, end_g1
-    #         k[edge_indices] = edge_aff[b, :ne1[b], :ne2[b]].reshape((-1,))
-    #     k = k.reshape((n2max * n1max, n2max * n1max))
-    #     # node-wise affinity
-    #     if node_aff is not None:
-    #         k_diag = paddle.diagonal(k)
-    #         k_diag[:] = node_aff[b].transpose((1, 0)).reshape((-1,))
-    #     ks.append(k)
-
-    # return paddle.stack(ks, axis=0)
-
 
 def _check_data_type(input: paddle.Tensor):
     """
