1f3202efad6efcf76eaec72c5810812f53d2c01b
roger <runzhong.wang@sjtu.edu.cn>
2022-07-14 01:28:03 +0800

    fix linear assignment bugs and add testing script

--- pygmtools/pytorch_backend.py
+++ pygmtools/pytorch_backend.py
@@ -57,9 +57,23 @@ def sinkhorn(s: Tensor, nrows: Tensor=None, ncols: Tensor=None,
         transposed = True
 
     if nrows is None:
-        nrows = [s.shape[1] for _ in range(batch_size)]
+        nrows = torch.tensor([s.shape[1] for _ in range(batch_size)], device=s.device)
     if ncols is None:
-        ncols = [s.shape[2] for _ in range(batch_size)]
+        ncols = torch.tensor([s.shape[2] for _ in range(batch_size)], device=s.device)
+
+    # ensure that in each dimension we have nrow < ncol
+    transposed_batch = nrows > ncols
+    if torch.any(transposed_batch):
+        s_t = s.transpose(1, 2)
+        s_t = torch.cat((
+            s_t[:, :s.shape[1], :],
+            torch.full((batch_size, s.shape[1], s.shape[2]-s.shape[1]), -float('inf'), device=s.device)), dim=2)
+        s = torch.where(transposed_batch.view(batch_size, 1, 1), s_t, s)
+
+        new_nrows = torch.where(transposed_batch, ncols, nrows)
+        new_ncols = torch.where(transposed_batch, nrows, ncols)
+        nrows = new_nrows
+        ncols = new_ncols
 
     # operations are performed on log_s
     s = s / tau
@@ -89,15 +103,7 @@ def sinkhorn(s: Tensor, nrows: Tensor=None, ncols: Tensor=None,
                 log_s = log_s - log_sum
                 log_s[torch.isnan(log_s)] = -float('inf')
 
-        if dummy_row and dummy_shape[1] > 0:
-            log_s = log_s[:, :-dummy_shape[1]]
-            for b in range(batch_size):
-                log_s[b, ori_nrows[b]:nrows[b], :ncols[b]] = -float('inf')
-
-        if transposed:
-            log_s = log_s.transpose(1, 2)
-
-        return torch.exp(log_s)
+        ret_log_s = log_s
     else:
         ret_log_s = torch.full((batch_size, s.shape[1], s.shape[2]), -float('inf'), device=s.device, dtype=s.dtype)
 
@@ -116,16 +122,23 @@ def sinkhorn(s: Tensor, nrows: Tensor=None, ncols: Tensor=None,
 
             ret_log_s[b, row_slice, col_slice] = log_s
 
-        if dummy_row:
-            if dummy_shape[1] > 0:
-                ret_log_s = ret_log_s[:, :-dummy_shape[1]]
-            for b in range(batch_size):
-                ret_log_s[b, ori_nrows[b]:nrows[b], :ncols[b]] = -float('inf')
+    if dummy_row:
+        if dummy_shape[1] > 0:
+            ret_log_s = ret_log_s[:, :-dummy_shape[1]]
+        for b in range(batch_size):
+            ret_log_s[b, ori_nrows[b]:nrows[b], :ncols[b]] = -float('inf')
 
-        if transposed:
-            ret_log_s = ret_log_s.transpose(1, 2)
+    if torch.any(transposed_batch):
+        s_t = ret_log_s.transpose(1, 2)
+        s_t = torch.cat((
+            s_t[:, :ret_log_s.shape[1], :],
+            torch.full((batch_size, ret_log_s.shape[1], ret_log_s.shape[2]-ret_log_s.shape[1]), -float('inf'), device=s.device)), dim=2)
+        ret_log_s = torch.where(transposed_batch.view(batch_size, 1, 1), s_t, ret_log_s)
 
-        return torch.exp(ret_log_s)
+    if transposed:
+        ret_log_s = ret_log_s.transpose(1, 2)
+
+    return torch.exp(ret_log_s)
 
 
 #############################################
