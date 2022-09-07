811427b64d445f9334af5c551607ff0075e1abf7
roger <runzhong.wang@sjtu.edu.cn>
2022-09-07 19:35:37 +0800

    version 0.2.6

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ DESCRIPTION = 'pygmtools provides graph matching solvers in Python API and suppo
               'pygmtools also provides dataset API for standard graph matching benchmarks.'
 URL = 'https://pygmtools.readthedocs.io/'
 AUTHOR = 'ThinkLab at SJTU'
-VERSION = '0.2.5'
+VERSION = '0.2.6'
 
 REQUIRED = [
      'requests>=2.25.1', 'scipy>=1.4.1', 'Pillow>=7.2.0', 'numpy>=1.18.5', 'easydict>=1.7',
