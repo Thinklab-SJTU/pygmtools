18f9288ab3f9ad5b8760ec7455884922a5f6f3b3
roger <runzhong.wang@sjtu.edu.cn>
2022-09-15 01:59:28 +0800

    move linear solvers from classic_solvers

--- pygmtools/__init__.py
+++ pygmtools/__init__.py
@@ -1,5 +1,6 @@
 from .benchmark import Benchmark
-from .classic_solvers import sinkhorn, hungarian, rrwm, sm, ipfp
+from .linear_solvers import sinkhorn, hungarian
+from .classic_solvers import rrwm, sm, ipfp
 from .multi_graph_solvers import cao, mgm_floyd, gamgm
 from .neural_solvers import pca_gm, ipca_gm, cie, ngm
 import pygmtools.utils as utils
