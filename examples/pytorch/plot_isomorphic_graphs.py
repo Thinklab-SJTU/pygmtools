a277fdc4adf8f6a310f90e700e5b67c910d9842e
roger <18309862+rogerwwww@users.noreply.github.com>
2022-10-13 22:13:13 +0800

    update isomorphic graph example

--- examples/pytorch/plot_isomorphic_graphs.py
+++ examples/pytorch/plot_isomorphic_graphs.py
@@ -4,7 +4,7 @@
 Introduction: Matching Isomorphic Graphs
 ========================================
 This example is an introduction to ``pygmtools`` which shows how to match isomorphic graphs.
-Isomorphic graphs means graphs whose structure are identical, but the node correspondence is unknown.
+Isomorphic graphs means graphs whose structures are identical, but the node correspondence is unknown.
 """
 
 # Author: Runzhong Wang
@@ -12,19 +12,27 @@ Isomorphic graphs means graphs whose structure are identical, but the node corre
 # License: Mulan PSL v2 License
 # sphinx_gallery_thumbnail_number = 6
 
+##############################################################################
+# .. note::
+#     The following solvers support QAP formulation, and are included in this example:
+#
+#     * :func:`~pygmtools.classic_solvers.rrwm` (classic solver)
+#     * :func:`~pygmtools.classic_solvers.ipfp` (classic solver)
+#     * :func:`~pygmtools.classic_solvers.sm` (classic solver)
+#     * :func:`~pygmtools.neural_solvers.ngm` (neural network solver)
+#
 import torch # pytorch backend
-import pygmtools as pygm #
+import pygmtools as pygm
 import matplotlib.pyplot as plt # for plotting
 from matplotlib.patches import ConnectionPatch # for plotting matching result
 import networkx as nx # for plotting graphs
-pygm.BACKEND = 'pytorch'
-_ = torch.manual_seed(1)
+pygm.BACKEND = 'pytorch' # set default backend for pygmtools
+_ = torch.manual_seed(1) # fix random seed
 
 ##############################################################################
 # Generate two isomorphic graphs
 # ------------------------------------
 #
-
 num_nodes = 10
 X_gt = torch.zeros(num_nodes, num_nodes)
 X_gt[torch.arange(0, num_nodes, dtype=torch.int64), torch.randperm(num_nodes)] = 1
@@ -75,6 +83,9 @@ K = pygm.utils.build_aff_mat(None, edge1, conn1, None, edge2, conn2, n1, None, n
 ##############################################################################
 # Visualization of the affinity matrix. For graph matching problem with :math:`N` nodes, the affinity matrix
 # has :math:`N^2\times N^2` elements because there are :math:`N^2` edges in each graph.
+#
+# .. note::
+#     The diagonal elements of the affinity matrix is empty because there is no node features in this example.
 #
 plt.figure(figsize=(4, 4))
 plt.title(f'Affinity Matrix (size: {K.shape[0]}$\\times${K.shape[1]})')
@@ -131,7 +142,7 @@ nx.draw_networkx(G2, pos=pos2)
 for i in range(num_nodes):
     j = torch.argmax(X[i]).item()
     con = ConnectionPatch(xyA=pos1[i], xyB=pos2[j], coordsA="data", coordsB="data",
-                          axesA=ax1, axesB=ax2, color="red")
+                          axesA=ax1, axesB=ax2, color="green")
     plt.gca().add_artist(con)
 
 ##############################################################################
@@ -149,7 +160,7 @@ for i in range(num_nodes):
     j = torch.argmax(X[i]).item()
     align_pos2[j] = pos1[i]
     con = ConnectionPatch(xyA=pos1[i], xyB=align_pos2[j], coordsA="data", coordsB="data",
-                          axesA=ax1, axesB=ax2, color="red")
+                          axesA=ax1, axesB=ax2, color="green")
     plt.gca().add_artist(con)
 nx.draw_networkx(G2, pos=align_pos2)
 
