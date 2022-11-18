2f855d9e1b27f6b561655bdb257b973536aa47fa
roger <18309862+rogerwwww@users.noreply.github.com>
2022-11-18 21:31:17 +0800

    temporarily remove jittor examples with model loading

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,6 @@ import datetime
 sys.path.insert(0, os.path.abspath('..'))
 
 os.environ['SPHINX'] = '1'
-os.environ['FIX_TORCH_ERROR'] = '1'
 
 # -- General configuration ------------------------------------------------
 
