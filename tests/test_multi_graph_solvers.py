c606c756c5f81e8c55f283cc58b041ce283bb7d8
roger <18309862+rogerwwww@users.noreply.github.com>
2022-11-19 22:20:10 +0800

    allow retry in cao test

--- tests/test_multi_graph_solvers.py
+++ tests/test_multi_graph_solvers.py
@@ -137,7 +137,8 @@ def _test_mgm_solver_on_isomorphic_graphs(num_graph, num_node, node_feat_dim, so
 def test_cao():
     num_nodes = 5
     num_graphs = 10
-    _test_mgm_solver_on_isomorphic_graphs(num_graphs, num_nodes, 10, pygm.cao, 'lawler-qap', {
+    max_retries = 5
+    args = (num_graphs, num_nodes, 10, pygm.cao, 'lawler-qap', {
         'mode': ['time', 'memory'],
         'x0': [None, 0.2, 0.5],
         'lambda_init': [0.1, 0.3],
@@ -145,6 +146,17 @@ def test_cao():
         'edge_aff_fn': [functools.partial(pygm.utils.gaussian_aff_fn, sigma=1.), pygm.utils.inner_prod_aff_fn],
         'node_aff_fn': [functools.partial(pygm.utils.gaussian_aff_fn, sigma=.1), pygm.utils.inner_prod_aff_fn]
     }, ['pytorch', 'paddle', 'jittor'])
+    for i in range(max_retries - 1):
+        error_flag = False
+        try:
+            _test_mgm_solver_on_isomorphic_graphs(*args)
+        except AssertionError as err:
+            print('Error caught (might be caused by randomness), retrying:\n', err)
+            error_flag = True
+        if not error_flag:
+            break
+    if error_flag:
+        _test_mgm_solver_on_isomorphic_graphs(*args)
 
 
 def test_mgm_floyd():
