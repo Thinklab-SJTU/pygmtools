049bd198e33c2ea1fc2b1db62ee2985b87b83694
wlx_glb <wlxlatiao_glb@163.com>
2023-04-11 16:03:28 +0800

    mindspore 4.11

--- tests/test1.py
+++ tests/test1.py
@@ -1,4 +1,14 @@
 import mindspore
+import sys
 
+sys.path.insert(0, '.')
+
+import numpy as np
+import torch
+import functools
+import itertools
+from tqdm import tqdm
+
+from test_utils import *
 a = mindspore.Tensor([0])
 print('a=', a)
