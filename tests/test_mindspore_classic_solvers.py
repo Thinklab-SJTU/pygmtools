4a2369112262d06d03424d4ba6c8bef611618ea8
wlx_glb <wlxlatiao_glb@163.com>
2023-04-19 23:31:09 +0800

    mindspore 4.19

--- tests/test_mindspore_classic_solvers.py
+++ tests/test_mindspore_classic_solvers.py
@@ -271,15 +271,6 @@ def test_rrwm():
         'alpha': [0.1, 0.5],
         'beta': [0.1, 1],
         'sk_iter': [10, 20],
-        'max_iter': [50],
-        'edge_aff_fn': [functools.partial(pygm.utils.gaussian_aff_fn, sigma=1.), pygm.utils.inner_prod_aff_fn],
-        'node_aff_fn': [functools.partial(pygm.utils.gaussian_aff_fn, sigma=.1), pygm.utils.inner_prod_aff_fn]
-    }, backends)
-
-    _test_classic_solver_on_isomorphic_graphs(list(range(10, 30, 2)), 10, pygm.rrwm, {
-        'alpha': [0.5, 0.9],
-        'beta': [1, 10],
-        'sk_iter': [10, 20],
         'max_iter': [20],
         'edge_aff_fn': [functools.partial(pygm.utils.gaussian_aff_fn, sigma=1.), pygm.utils.inner_prod_aff_fn],
         'node_aff_fn': [functools.partial(pygm.utils.gaussian_aff_fn, sigma=.1), pygm.utils.inner_prod_aff_fn]
