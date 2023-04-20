a66e78fe7517c809fa5b754aca0c00826ac0152e
wlx_glb <wlxlatiao_glb@163.com>
2023-04-20 14:28:00 +0800

    mindspore 4.20

--- tests/test_mindspore_classic_solvers.py
+++ tests/test_mindspore_classic_solvers.py
@@ -267,14 +267,14 @@ def test_sinkhorn():
 
 
 def test_rrwm():
-    # _test_classic_solver_on_isomorphic_graphs(list(range(10, 30, 2)), 10, pygm.rrwm, {
-    #     'alpha': [0.1, 0.5],
-    #     'beta': [0.1, 1],
-    #     'sk_iter': [10, 20],
-    #     'max_iter': [20, 50],
-    #     'edge_aff_fn': [functools.partial(pygm.utils.gaussian_aff_fn, sigma=1.), pygm.utils.inner_prod_aff_fn],
-    #     'node_aff_fn': [functools.partial(pygm.utils.gaussian_aff_fn, sigma=.1), pygm.utils.inner_prod_aff_fn]
-    # }, backends)
+    _test_classic_solver_on_isomorphic_graphs(list(range(10, 30, 2)), 10, pygm.rrwm, {
+        'alpha': [0.1, 0.5],
+        'beta': [0.1, 1],
+        'sk_iter': [10, 20],
+        'max_iter': [20, 50],
+        'edge_aff_fn': [functools.partial(pygm.utils.gaussian_aff_fn, sigma=1.), pygm.utils.inner_prod_aff_fn],
+        'node_aff_fn': [functools.partial(pygm.utils.gaussian_aff_fn, sigma=.1), pygm.utils.inner_prod_aff_fn]
+    }, backends)
 
     # non-batched input
     _test_classic_solver_on_isomorphic_graphs([10], 10, pygm.rrwm, {
@@ -288,11 +288,11 @@ def test_rrwm():
 
 
 def test_sm():
-    # _test_classic_solver_on_isomorphic_graphs(list(range(10, 30, 2)), 10, pygm.sm, {
-    #     'max_iter': [10, 50, 100],
-    #     'edge_aff_fn': [functools.partial(pygm.utils.gaussian_aff_fn, sigma=1.), pygm.utils.inner_prod_aff_fn],
-    #     'node_aff_fn': [functools.partial(pygm.utils.gaussian_aff_fn, sigma=.1), pygm.utils.inner_prod_aff_fn]
-    # }, backends)
+    _test_classic_solver_on_isomorphic_graphs(list(range(10, 30, 2)), 10, pygm.sm, {
+        'max_iter': [10, 50, 100],
+        'edge_aff_fn': [functools.partial(pygm.utils.gaussian_aff_fn, sigma=1.), pygm.utils.inner_prod_aff_fn],
+        'node_aff_fn': [functools.partial(pygm.utils.gaussian_aff_fn, sigma=.1), pygm.utils.inner_prod_aff_fn]
+    }, backends)
 
     # non-batched input
     _test_classic_solver_on_isomorphic_graphs([10], 10, pygm.sm, {
