ff01a0f44f19eab67c6de92aacbfbbc3f54b3658
heatingma <115260102+heatingma@users.noreply.github.com>
2023-07-27 16:00:42 +0800

    add genn_astar

--- pygmtools/__init__.py
+++ pygmtools/__init__.py
@@ -12,7 +12,7 @@ from .benchmark import Benchmark
 from .linear_solvers import sinkhorn, hungarian
 from .classic_solvers import rrwm, sm, ipfp, astar
 from .multi_graph_solvers import cao, mgm_floyd, gamgm
-from .neural_solvers import pca_gm, ipca_gm, cie, ngm
+from .neural_solvers import pca_gm, ipca_gm, cie, ngm, genn_astar
 import pygmtools.utils as utils
 BACKEND = 'numpy'
 __version__ = '0.3.8'
